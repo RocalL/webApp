dd906ebbedf024c7a95b5a1cbdd780cb3a052f3f
Ranaivosoa <root@MonAmi>
2018-05-15 16:24:06 +0200

    [Ranaivosoa] CreationProjet

--- WebContent/WEB-INF/views/creationProjet.jsp
+++ WebContent/WEB-INF/views/creationProjet.jsp
@@ -5,5 +5,49 @@
 
 <%@include file="./navigation.jspf"%>
 
-<div class="container"></div>
+<div class="container">
+	<div class="row">
+		<h1 class="custom-title">Création d'un Projet</h1>
+	</div>
+	<div class="row" id="projet">
+		<div class="col-lg-12" id="form">
+			<form method="post" action="creationProjet">
+				<div class="form-group">
+						    <label for="nomProjet">Nom du Projet<span class="requis">*</span></label> 
+							<input type="text" class="form-control" id="nomProjet" name="nomProjet" placeholder="Ex: Création d'une apllication web">
+							<span class="erreur">${form.erreurs['Nom du projet']}</span>
+							
+							<label for="descriptifProjet">Descriptif<span class="requis">*</span></label>
+							<input type="text" class="form-control" id="descriptifProjet" name="descriptifProjet" placeholder="détail du projet">
+							<span class="erreur">${form.erreurs['descriptif du projet']}</span>
+							
+							<label for="deadLineCandidature">Date Limite de candidature<span class="requis">*</span></label>
+							<input type="date" class="form-control" id="deadLineCandidature" name="deadLineCandidature" placeholder="01/01/2000"> 
+							<span class="erreur">${form.erreurs['date limite de candidature']}</span>
+							
+							<label for="deadLineProjet">Date de cloture du projet<span class="requis">*</span></label>
+							<input type="date" class="form-control" id="deadLineProjet" name="deadLineProjet" placeholder="01/01/2000"> 
+							<span class="erreur">${form.erreurs['date de cloture du projet']}</span>
+							
+							<label for="nbMaxCandidatures">Nombre de développeur(s) nécessaire(s)<span class="requis">*</span></label>
+							<input type="number" class="form-control" id="deadLineProjet" name="deadLineProjet" placeholder="01"> 
+							<span class="erreur">${form.erreurs['Nombre de développeur']}</span>
+							
+							<label for="ImageProjet">Image d'illustration</label>
+							<div class="input1" id="ImageProjet">
+								<div class="custom-file">
+									<input type="file" class="custom-file-input" id="fichiers" name="fichiers">
+									<label class="custom-file-label" for="fichiers">Charger une image</label>															
+								</div>
+							</div>
+							<small id="fichierWarn" class="form-text text-muted">veuillez importer une image valide</small>				
+					</div>
+					<div id="sendButton">
+						<button type="submit" class="btn">Soumettre le projet</button>
+					</div>
+					<p class="${empty form.erreurs ? 'succes' : 'erreur'}">${form.resultat}</p>
+			</form>
+		</div>
+</div>
+</div>
 <%@include file="./footer.jspf"%>
---
+++ src/forms/CreationProjet.java
@@ -0,0 +1,211 @@
+package forms;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import model.Candidature;
+import model.Projet;
+import model.RepProjet;
+import model.Structure;
+import model.Utilisateur;
+import services.Projets;
+import util.JaxParser;
+
+public class CreationProjet {
+	private static final String CHAMP_RAISONSOCIALE = "raisonSociale";
+	private static final String CHAMP_SIRET = "siret";
+	private static final String CHAMP_CA = "ca";
+	private static final String CHAMP_WEBSITE = "website";
+	private static final String CHAMP_DEVIS = "devis";
+	private static final String CHAMP_FICHIERS = "fichiers";
+	private static final String CHAMP_DELAI = "delai";
+	public static final String ATT_SESSION_USER = "sessionUtilisateur";
+
+	private String resultat;
+	private Map<String, String> erreurs = new HashMap<String, String>();
+
+	public Map<String, String> getErreurs() {
+		return erreurs;
+	}
+
+	public String getResultat() {
+		return resultat;
+	}
+
+	public Candidature creerCandidature(HttpServletRequest request, String chemin) {
+		HttpSession session = request.getSession();
+		Utilisateur utilisateur = (Utilisateur) session.getAttribute(ATT_SESSION_USER);
+
+		String raisonSociale = getValeurChamp(request, CHAMP_RAISONSOCIALE);
+		String siret = getValeurChamp(request, CHAMP_SIRET);
+		String ca = getValeurChamp(request, CHAMP_CA);
+
+		String delai = getValeurChamp(request, CHAMP_DELAI);
+		String website = getValeurChamp(request, CHAMP_WEBSITE);
+		String devis = getValeurChamp(request, CHAMP_DEVIS);
+		String fichiers = getValeurChamp(request, CHAMP_FICHIERS);
+
+		Candidature candidature = new Candidature();
+
+		Structure structure = new Structure();
+		RepProjet repProjet = new RepProjet();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+		Date date = new Date();
+
+		try {
+			validationRaisonSociale(raisonSociale);
+		} catch (Exception e) {
+			setErreur(CHAMP_RAISONSOCIALE, e.getMessage());
+		}
+		structure.setRaisonSocial(raisonSociale);
+
+		long siretLong = -1;
+		try {
+			siretLong = validationSiret(siret);
+		} catch (Exception e) {
+			setErreur(CHAMP_SIRET, e.getMessage());
+		}
+		structure.setSiret(siretLong);
+
+		int caInt = -1;
+		try {
+			caInt = validationCa(ca);
+		} catch (Exception e) {
+			setErreur(CHAMP_CA, e.getMessage());
+		}
+		structure.setCa(caInt);
+
+		int delaiInt = -1;
+		try {
+			delaiInt = validationDelai(delai);
+		} catch (Exception e) {
+			setErreur(CHAMP_DELAI, e.getMessage());
+		}
+		repProjet.setDelaisPropose(delaiInt);
+
+		candidature.setUtilisateur(utilisateur);
+		candidature.setStructure(structure);
+		candidature.setRepProjet(repProjet);
+		candidature.setDateCandidature(dateFormat.format(date));
+
+		try {
+			File file = new File(request.getServletContext().getRealPath(chemin));
+			if (erreurs.isEmpty()) {
+				// Read
+				Projets listProjets = JaxParser.unmarshal(Projets.class, file);
+				for (Projet p : listProjets.getProjet()) {
+					if (p.getNom().equals(request.getParameter("projet"))) {
+						p.addCandidature(candidature);
+					}
+				}
+				// Write
+				JaxParser.marshal(listProjets, file);
+				System.out.println(candidature);
+				System.out.println("ajoutée au projet");
+			}
+		} catch (Exception e) {
+			setErreur("ParserError", e.getMessage());
+			e.printStackTrace();
+		}
+
+		if (erreurs.isEmpty()) {
+			resultat = "Succès de la création de la candidature le " + date.toString();
+		} else {
+			resultat = "échec de la création de la candidature.";
+		}
+
+		return candidature;
+	}
+
+	private void validationRaisonSociale(String raisonSociale) throws Exception {
+		if (raisonSociale != null) {
+			if (raisonSociale.length() < 2) {
+				throw new Exception("Le champ raison sociale doit contenir au moins 2 caract�res.");
+			}
+		} else {
+			throw new Exception("Merci d'entrer une raison sociale.");
+		}
+	}
+
+	private long validationSiret(String siret) throws Exception {
+		long temp;
+		if (siret != null) {
+			try {
+				temp = Long.parseLong(siret);
+				if (siret.length() < 1) { // 14
+					throw new Exception("Le siret doit contenir au moins 14 caract�res.");
+				}
+			} catch (NumberFormatException e) {
+				temp = -1;
+				throw new Exception("Le siret doit �tre un nombre.");
+			}
+		} else {
+			temp = -1;
+			throw new Exception("Merci d'entrer un siret.");
+		}
+		return temp;
+	}
+
+	private int validationCa(String ca) throws Exception {
+		int temp;
+		if (ca != null) {
+			try {
+				temp = Integer.parseInt(ca);
+				if (temp < 0) {
+					throw new Exception("Le chiffre d'affaire doit être un nombre positif.");
+				}
+			} catch (NumberFormatException e) {
+				temp = -1;
+				throw new Exception("Le chiffre d'affaire doit être un nombre.");
+			}
+		} else {
+			temp = -1;
+			throw new Exception("Merci d'entrer un chiffre d'affaire.");
+		}
+		return temp;
+	}
+
+	private int validationDelai(String delai) throws Exception {
+		int temp;
+		if (delai != null) {
+			try {
+				temp = Integer.parseInt(delai);
+				if (temp < 0) {
+					throw new Exception("Le délai doit être un nombre positif.");
+				}
+			} catch (NumberFormatException e) {
+				temp = -1;
+				throw new Exception("Le délai doit être un nombre.");
+			}
+		} else {
+			temp = -1;
+			throw new Exception("Merci d'entrer un délai.");
+		}
+		return temp;
+	}
+
+	/*
+	 * Ajoute un message correspondant au champ sp�cifi� � la map des erreurs.
+	 */
+	private void setErreur(String champ, String message) {
+		erreurs.put(champ, message);
+	}
+
+	/*
+	 * M�thode utilitaire qui retourne null si un champ est vide, et son contenu
+	 * sinon.
+	 */
+	private static String getValeurChamp(HttpServletRequest request, String nomChamp) {
+		String valeur = request.getParameter(nomChamp);
+		if (valeur == null || valeur.trim().length() == 0) {
+			return null;
+		} else {
+			return valeur;
+		}
+	}
+}
